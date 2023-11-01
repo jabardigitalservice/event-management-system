c234e3ce724352d8b835d8a6e88ba377e31ad7c1
johndy2742 <johndypanca12345@gmail.com>
2023-11-01 14:34:28 +0700

    use RenderError function to handle error response

--- service/src/modules/object/transport/handler/update_object.go
+++ service/src/modules/object/transport/handler/update_object.go
@@ -2,12 +2,10 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/google/uuid"
-	_errors "github.com/jabardigitalservice/super-app-services/event/src/error"
 	helperresponse "github.com/jabardigitalservice/super-app-services/event/src/helper/http/response"
 	"github.com/jabardigitalservice/super-app-services/event/src/modules/object/transport/handler/http/request"
 	httpresponse "github.com/jabardigitalservice/super-app-services/event/src/response/http"
@@ -17,13 +15,13 @@ func (h *Handler) UpdateObject(w http.ResponseWriter, r *http.Request) {
 	idStr := chi.URLParam(r, "id")
 	id, err := uuid.Parse(idStr)
 	if err != nil {
-		http.Error(w, "Invalid ID parameter", http.StatusBadRequest)
+		RenderError(w, r, h, err, nil)
 		return
 	}
 
 	var obj request.Object
 	if err := json.NewDecoder(r.Body).Decode(&obj); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		RenderError(w, r, h, err, nil)
 		return
 	}
 
@@ -33,12 +31,7 @@ func (h *Handler) UpdateObject(w http.ResponseWriter, r *http.Request) {
 
 	updatedObj, err := h.endpoint.UpdateObject(ctx, &obj)
 	if err != nil {
-		if errors.Is(err, _errors.ErrNotFound) {
-			http.Error(w, "Object not found", http.StatusNotFound)
-			return
-		}
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		RenderError(w, r, h, err, nil)
 		return
 	}
 

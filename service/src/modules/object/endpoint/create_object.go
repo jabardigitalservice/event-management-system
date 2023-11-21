5f43521e3762dda03c15defb75e5e3f0ecf950d2
johndy2742 <johndypanca12345@gmail.com>
2023-11-21 13:18:16 +0700

    init new relic in endpoint

--- service/src/modules/object/endpoint/create_object.go
+++ service/src/modules/object/endpoint/create_object.go
@@ -11,7 +11,7 @@ import (
 )
 
 func (e *Endpoint) CreateObject(ctx context.Context, objData request.Object) (interface{}, error) {
-	endpointSegment := e.StartSegment(ctx, "CreateObjectEndpoint")
+	endpointSegment := e.newrelic.StartSegment(ctx, "CreateObjectEndpoint")
 
 	var validates = validator.Validate(objData)
 
